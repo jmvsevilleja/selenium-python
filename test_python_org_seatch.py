6b2ae66c3ecbf5ef64738a3f7a29d185a6f58fc5
jmvsevilleja <jmvsevilleja@gmai.com>
2019-04-18 21:51:23 +0800

    Added chrome driver install

--- test_python_org_seatch.py
+++ test_python_org_seatch.py
@@ -1,19 +1,22 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import chromedriver_install as cdi
 
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        path = cdi.install(file_directory='./lib/', verbose=True,
+                           chmod=True, overwrite=False, version=None)
+        self.driver = webdriver.Chrome(path)
 
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://www.python.org")
-        self.assertIn("Pythons", driver.title)
+        self.assertIn("Python", driver.title)
         elem = driver.find_element_by_name("q")
-        elem.send_keys("pyconsss")
+        elem.send_keys("pycon")
         elem.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
 
